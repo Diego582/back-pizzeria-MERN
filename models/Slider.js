38ab760ad49bf7e5cc6b5fe2160f52434330675e
Diego <penaomardiego@gmail.com>
2023-10-10 09:59:06 -0300

    config

--- models/Slider.js
+++ models/Slider.js
@@ -7,7 +7,7 @@ let schema = new Schema({
     title: { type: String, required: true, default:'Delicious' },
     description: { type: String, required: true, default: 'food for health'  },
     paragraph: { type: String, required: true, default:"We work closely with our team of artisan  in Italy utilizing sustainable  materials and processes"},
-    product_id: { type: Schema.Types.ObjectId, required: true, ref: "Products" },
+    product_id: { type: Schema.Types.ObjectId, required: true, ref: "products" },
 
 })
 let Slider = model(collection, schema)
