d1a61e604d08162f29e9f0692f6e486ac9b3e276
Fara <102741815+Farahh7@users.noreply.github.com>
2023-10-24 15:34:48 -0300

    fix reviews

--- controllers/reviews/read.js
+++ controllers/reviews/read.js
@@ -1,4 +1,37 @@
-import https from 'https';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default async (req, res) => {
+    try {
+        const placeId = process.env.GOOGLE_PLACE_ID;
+        const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+        const apiUrl = `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&fields=reviews&key=${apiKey}`;
+
+        const response = await axios.get(apiUrl);
+        console.log(apiUrl);
+
+        if (response.data.result.reviews) {
+            
+            return res.status(200).json({
+                success: true,
+                message: "Reviews found",
+                response: response.data.result.reviews,
+              });
+        } else {
+            res.status(404).json({ message: 'No reviews found' });
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error fetching reviews' });
+    }
+
+};
+
+
+/* import https from 'https';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -35,7 +68,7 @@ export default async (req, res) => {
         console.error(error);
         res.status(500).json({ message: 'Error fetching reviews' });
     }
-};
+}; */
  
 /* 
  import axios from 'axios';
